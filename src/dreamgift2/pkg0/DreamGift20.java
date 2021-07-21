2008868fe35ce467d7bef9f9204f598d7c612940
CotoF <CotoF@DESKTOP-KS1U7T1>
2021-07-21 16:30:19 -0400

    302

---
+++ src/dreamgift2/pkg0/DreamGift20.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dreamgift2.pkg0;
+
+/**
+ *
+ * @author CotoF
+ */
+public class DreamGift20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
